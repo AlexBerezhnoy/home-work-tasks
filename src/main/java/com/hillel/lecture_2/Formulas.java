64c7f74defb67abec16c65c305c6d538e09bcc04
AlexBerezhniy <dinnylol221@gmail.com>
2020-03-01 20:40:47 +0200

    add homework for lecture_2

--- src/main/java/com/hillel/lecture_2/Formulas.java
+++ src/main/java/com/hillel/lecture_2/Formulas.java
@@ -9,35 +9,35 @@ public class Formulas {
 
     @Step
     public double calculate9Formula(double x) {
-        double result = 0.0;
+        double result = 1.1*Math.pow(Math.E, -1*x)+Math.abs(Math.cos(Math.sqrt(Math.PI*x)))-3.0/8.0;
 //        TODO implement formula 9
         return result;
     }
 
     @Step
     public double calculate10Formula(double x) {
-        double result = 0.0;
+        double result = (1.0/3.0)*Math.sqrt(Math.abs(Math.sin(x))) * Math.pow(Math.pow(Math.E,0.12*x),1.0/3.0);
 //        TODO implement formula 10
         return result;
     }
 
     @Step
     public double calculate11Formula(double x) {
-        double result = 0.0;
+        double result = 2*Math.PI*x-Math.abs(Math.sin(Math.sqrt(10.5*x)))/(Math.pow(x*x,1.0/3.0)+1.0/7.0);
 //        TODO implement formula 11
         return result;
     }
 
     @Step
     public double calculate12Formula(double x) {
-        double result = 0.0;
+        double result =Math.log(Math.sqrt(Math.abs(2-x))+1.2)/(2+Math.pow(Math.E,-1*x))+Math.pow(2.0/x,1.0/3.0);
 //        TODO implement formula 12
         return result;
     }
 
     @Step
     public double calculate13Formula(double x) {
-        double result = 0.0;
+        double result =Math.pow(Math.exp(-2+x),1.0/5.0)/Math.sqrt(Math.pow(x,2)+Math.pow(x,4)+Math.log(Math.abs(x-3.14)));
 //        TODO implement formula 13
         return result;
     }
--- src/main/java/com/hillel/lecture_2/TemperatureConverter.java
+++ src/main/java/com/hillel/lecture_2/TemperatureConverter.java
@@ -10,21 +10,22 @@ public class TemperatureConverter {
     @Step
     public double celsiusToFahrenheit(double celsius) {
 //       TODO implements result Convert Celsius to Fahrenheit
-        double result = 0.0;
+        double result = (celsius * 9.0/5.0) + 32.0;
+
         return result;
     }
 
     @Step
     public double fahrenheitToCelsius(double fahrenheit) {
 //        TODO implements result convert Fahrenheit to Celsius
-        double result = 0.0;
+        double result = (fahrenheit - 32.0) * 5.0/9.0;
         return result;
     }
 
     @Step
     public double celsiusToKelvin(double celsius) {
 //        TODO implements result convert Celsius to Kelvin
-        double result = 0.0;
+        double result =  celsius + 273.16;
         return result;
     }
 }
