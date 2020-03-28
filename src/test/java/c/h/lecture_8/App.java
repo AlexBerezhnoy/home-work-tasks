d7d65488d66ec93864e21b0e50d6bb8921769ca6
Olga Berezhna <berezhna.o.v@gmail.com>
2020-03-28 22:37:27 +0200

    add HM8

---
+++ src/test/java/c/h/lecture_8/App.java
@@ -0,0 +1,36 @@
+package c.h.lecture_8;
+
+public class App {
+    public static void main(String[] args) {
+        double expectedSumResult = 0.95;
+        double expectedSubResult = 0.55;
+        double expectedMultiResult = 2.25;
+        double expectedDivResult = 0.25;
+        double term = 0.2;
+        double subtrahend = 0.2;
+        double factor = 3;
+        double divider = 3;
+
+        FractionNumber fractionNumber = new FractionNumber(3,4);
+        System.out.println("Fraction Number object is  "+fractionNumber.toString() + '\n');
+        double actualResult = fractionNumber.addition(term);
+        validationResult(expectedSumResult,actualResult);
+        actualResult = fractionNumber.subtraction(subtrahend);
+        validationResult(expectedSubResult,actualResult);
+        actualResult = fractionNumber.multiplication(factor);
+        validationResult(expectedMultiResult,actualResult);
+        actualResult = fractionNumber.division(divider);
+        validationResult(expectedDivResult,actualResult);
+    }
+
+
+    public static void validationResult (double expectedResult, double actualResult) {
+        if (expectedResult == actualResult) {
+            System.out.println("The result is correct" + '\n');
+        } else {
+            System.out.println("The result isn't correct!!!");
+            System.out.println("Actual result :" + actualResult);
+            System.out.println("Expected result :" + expectedResult + '\n');
+        }
+    }
+}
---
+++ src/test/java/c/h/lecture_8/FractionNumber.java
@@ -0,0 +1,43 @@
+package c.h.lecture_8;
+
+public class FractionNumber {
+    private int numerator;
+    private int denominator;
+
+    FractionNumber(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public double addition (double term) {
+        double result =  (double) numerator/denominator + term;
+        System.out.println("Sum " + numerator + "/"+denominator + " + " + term + " = " + result);
+        return result;
+    }
+
+    public double subtraction (double subtrahend) {
+        double result = (double) numerator/denominator - subtrahend;
+        System.out.println("Sub " + numerator + "/"+denominator + " - " + subtrahend + " = " + result);
+        return result;
+    }
+
+    public double multiplication (double factor) {
+        double result = (double) numerator/denominator * factor;
+        System.out.println("Multi " + numerator + "/"+denominator + " * " + factor + " = " + result);
+        return  result;
+    }
+
+    public double division (double divider) {
+        double result =((double) numerator/denominator) / divider;
+        System.out.println("Div " + numerator + "/"+denominator + " : " + divider + " = " + result);
+        return  result;
+    }
+
+    @Override
+    public String toString() {
+        return "FractionNumbers{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+}
